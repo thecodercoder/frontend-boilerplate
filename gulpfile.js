4b6288d183388d9452763cedc7cc69cfcc4ac2ae
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2019-05-01 17:08:36 -0500

    Update Gulp packages for security fixes

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,8 @@ function cacheBustTask(){
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
 function watchTask(){
-    watch([files.scssPath, files.jsPath], parallel(scssTask, jsTask));    
+    watch([files.scssPath, files.jsPath], 
+        parallel(scssTask, jsTask));    
 }
 
 // Export the default Gulp task so it can be run
