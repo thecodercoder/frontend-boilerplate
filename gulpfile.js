9d3bb2041b564ede912f2bc3c6a4cfccd3d4d2dc
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2021-07-08 09:59:06 -0500

    Add Browsersync task to sync local server

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ const terser = require('gulp-terser');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-var replace = require('gulp-replace');
+const replace = require('gulp-replace');
+const browsersync = require('browser-sync').create();
 
 // File paths
 const files = {
@@ -46,6 +47,28 @@ function cacheBustTask() {
 		.pipe(dest('.'));
 }
 
+// Browsersync to spin up a local server
+function browserSyncServe(cb) {
+	// initializes browsersync server
+	browsersync.init({
+		server: {
+			baseDir: '.',
+		},
+		notify: {
+			styles: {
+				top: 'auto',
+				bottom: '0',
+			},
+		},
+	});
+	cb();
+}
+function browserSyncReload(cb) {
+	// reloads browsersync server
+	browsersync.reload();
+	cb();
+}
+
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
 function watchTask() {
@@ -56,7 +79,28 @@ function watchTask() {
 	);
 }
 
+// Browsersync Watch task
+// Watch HTML file for change and reload browsersync server
+// watch SCSS and JS files for changes, run scss and js tasks simultaneously and update browsersync
+function bsWatchTask() {
+	watch('index.html', browserSyncReload);
+	watch(
+		[files.scssPath, files.jsPath],
+		{ interval: 1000, usePolling: true }, //Makes docker work
+		series(parallel(scssTask, jsTask), cacheBustTask, browserSyncReload)
+	);
+}
+
 // Export the default Gulp task so it can be run
 // Runs the scss and js tasks simultaneously
 // then runs cacheBust, then watch task
 exports.default = series(parallel(scssTask, jsTask), cacheBustTask, watchTask);
+
+// Runs all of the above but also spins up a local Browsersync server
+// Run by typing in "gulp bs" on the command line
+exports.bs = series(
+	parallel(scssTask, jsTask),
+	cacheBustTask,
+	browserSyncServe,
+	bsWatchTask
+);
