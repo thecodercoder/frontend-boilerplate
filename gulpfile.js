4208b22b6c1409642f147bf5c97765c0fce908af
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2021-07-08 09:40:13 -0500

    Update packages, install sass
    Moved packages from devDependencies to dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@
 // Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
 const { src, dest, watch, series, parallel } = require('gulp');
 // Importing all the Gulp-related packages we want to use
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const terser = require('gulp-terser');
 const postcss = require('gulp-postcss');
@@ -19,23 +18,24 @@ const files = {
 
 // Sass task: compiles the style.scss file into style.css
 function scssTask() {
-	return src(files.scssPath)
-		.pipe(sourcemaps.init()) // initialize sourcemaps first
+	return src(files.scssPath, { sourcemaps: true }) // set source and turn on sourcemaps
 		.pipe(sass()) // compile SCSS to CSS
 		.pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
-		.pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-		.pipe(dest('dist')); // put final CSS in dist folder
+		.pipe(dest('dist', { sourcemaps: '.' })); // put final CSS in dist folder with sourcemap
 }
 
 // JS task: concatenates and uglifies JS files to script.js
 function jsTask() {
-	return src([
-		files.jsPath,
-		//,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
-	])
+	return src(
+		[
+			files.jsPath,
+			//,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+		],
+		{ sourcemaps: true }
+	)
 		.pipe(concat('all.js'))
 		.pipe(terser())
-		.pipe(dest('dist'));
+		.pipe(dest('dist', { sourcemaps: '.' }));
 }
 
 // Cachebust
