f368c8880d47ca135cef92920da32d0b7fdb3b7f
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2021-07-08 09:22:44 -0500

    Replace gulp-uglify with gulp-terser

--- gulpfile.js
+++ gulpfile.js
@@ -5,67 +5,58 @@ const { src, dest, watch, series, parallel } = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const terser = require('gulp-terser');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 var replace = require('gulp-replace');
 
-
 // File paths
-const files = { 
-    scssPath: 'app/scss/**/*.scss',
-    jsPath: 'app/js/**/*.js'
-}
+const files = {
+	scssPath: 'app/scss/**/*.scss',
+	jsPath: 'app/js/**/*.js',
+};
 
 // Sass task: compiles the style.scss file into style.css
-function scssTask(){    
-    return src(files.scssPath)
-        .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass()) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
-        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        .pipe(dest('dist')
-    ); // put final CSS in dist folder
+function scssTask() {
+	return src(files.scssPath)
+		.pipe(sourcemaps.init()) // initialize sourcemaps first
+		.pipe(sass()) // compile SCSS to CSS
+		.pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+		.pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+		.pipe(dest('dist')); // put final CSS in dist folder
 }
 
 // JS task: concatenates and uglifies JS files to script.js
-function jsTask(){
-    return src([
-        files.jsPath
-        //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
-        ])
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(dest('dist')
-    );
+function jsTask() {
+	return src([
+		files.jsPath,
+		//,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+	])
+		.pipe(concat('all.js'))
+		.pipe(terser())
+		.pipe(dest('dist'));
 }
 
 // Cachebust
-function cacheBustTask(){
-    var cbString = new Date().getTime();
-    return src(['index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest('.'));
+function cacheBustTask() {
+	var cbString = new Date().getTime();
+	return src(['index.html'])
+		.pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+		.pipe(dest('.'));
 }
 
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
-function watchTask(){
-    watch([files.scssPath, files.jsPath],
-        {interval: 1000, usePolling: true}, //Makes docker work
-        series(
-            parallel(scssTask, jsTask),
-            cacheBustTask
-        )
-    );    
+function watchTask() {
+	watch(
+		[files.scssPath, files.jsPath],
+		{ interval: 1000, usePolling: true }, //Makes docker work
+		series(parallel(scssTask, jsTask), cacheBustTask)
+	);
 }
 
 // Export the default Gulp task so it can be run
 // Runs the scss and js tasks simultaneously
 // then runs cacheBust, then watch task
-exports.default = series(
-    parallel(scssTask, jsTask), 
-    cacheBustTask,
-    watchTask
-);
+exports.default = series(parallel(scssTask, jsTask), cacheBustTask, watchTask);
